1e2983aff6092fad5051b6d03874b9627206397b
lilmaifun <lilmaifun@gmail.com>
2019-09-17 14:41:39 -0700

    reduce

--- HigherOrderFunction.js
+++ HigherOrderFunction.js
@@ -1,51 +1,51 @@
-// //Higher Order Function
-// function repeat(fn, n) {
+//Higher Order Function
+function repeat(fn, n) {
   
-//   for(let i=0; i<n; i++){
-//     fn();
-//   }
-// }
-// repeat(hello, 5);
-// repeat(goodbye, 6);
+  for(let i=0; i<n; i++){
+    fn();
+  }
+}
+repeat(hello, 5);
+repeat(goodbye, 6);
   
-// function hello() {
-//   console.log('Hello world');
-// }
+function hello() {
+  console.log('Hello world');
+}
   
-// function goodbye() {
-//   console.log('Goodbye world');
-// }
+function goodbye() {
+  console.log('Goodbye world');
+}
 
-// // filter 
+// filter 
 
-// function filter(arr, fn) {
-//   let newArray = [];
-//   for (let i = 0; i < arr.length; i++){
-//     if (fn(arr[i])===true){
-//       newArray.push(arr[i]);
-//     }
-//   }
-//   return newArray;
-// }
+function filter(arr, fn) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++){
+    if (fn(arr[i])===true){
+      newArray.push(arr[i]);
+    }
+  }
+  return newArray;
+}
   
-// // DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
-// // First we setup an array of strings we plan to filter:
-// const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+// DO NOT EDIT BETWEEN THESE LINES, BUT DO READ THE CODE ----->
+// First we setup an array of strings we plan to filter:
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
   
-// // We use your `filter` function here, capturing a new array into `filteredNames`
-// // comprised of names that only begin with 'R'
-// const filteredNames = filter(myNames, function(name) {
-//   // This is known as a "predicate function" - it's a function that 
-//   // only returns a boolean
-//   return name[0] === 'R';
-// });
+// We use your `filter` function here, capturing a new array into `filteredNames`
+// comprised of names that only begin with 'R'
+const filteredNames = filter(myNames, function(name) {
+  // This is known as a "predicate function" - it's a function that 
+  // only returns a boolean
+  return name[0] === 'R';
+});
   
-// console.log(filteredNames) // => ['Rich', 'Ray']
-// // <---- DO NOT EDIT BETWEEN THESE LINES
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
 
-// console.log(filter(myNames, name => name[0]==='R'))
+console.log(filter(myNames, name => name[0]==='R'))
 
-//Function as return Values
+// Function as return Values
 
 function hazardWarningCreator(typeOfWarning) {
     let warningCounter = 0;
@@ -81,3 +81,23 @@ console.log(addNum);
 
 const movements = addNum.forEach((el,i) => console.log(`Movement #${++i}: ${el} steps`))
 console.log(movements);
+
+function decode(message){
+ 
+  let msgArr = message.split(' ');
+  
+  let decoded = msgArr.reduce((accumulator, currentValue) => {
+    if (currentValue.length === 3){
+      return accumulator + " ";
+    } else {
+      return accumulator + currentValue.slice(-1).toUpperCase();
+    }
+  }, "");
+
+  return decoded;
+
+}
+
+let message = 'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
+console.log(decode(message));
